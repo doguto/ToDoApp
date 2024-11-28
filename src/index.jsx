0ddee9547847a663cc0833c28cfeeede229dea52
Takahiro Togi <37960481-doguto110@users.noreply.replit.com>
2024-11-28 15:50:37 +0000

    [Feat] basis of react Todo App

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import Todo from './todo'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+import Todo from "./todo";
 //import * as serviceWorker from './serviceWorker'
 
 //ReactDOM.render(<Todo />, document.getElementById('root'))
 
 //serviceWorker.unregister();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Todo />
-	</React.StrictMode>
-)
+	</React.StrictMode>,
+);
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,34 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 export default class Todo extends Component {
   constructor(props) {
     super(props);
     this.state = {
       todos: [],
-      name: ''
+      name: "",
     };
   }
 
   onInput = (e) => {
     this.setState({
-      name: e.target.value
+      name: e.target.value,
     });
-  }
+  };
 
   addTodo = () => {
-    const {todos, name} = this.state;
+    const { todos, name } = this.state;
     this.setState({
-      todos: [...todos, name]
+      todos: [...todos, name],
     });
-  }
+  };
 
   removeTodo = (index) => {
-    const {todos, name} = this.state;
+    const { todos, name } = this.state;
     this.setState({
-      todos: [...todos.slice(0,index), ...todos.slice(index+1)]
+      todos: [...todos.slice(0, index), ...todos.slice(index + 1)],
     });
-  }
-  
+  };
+
   render() {
     const { todos } = this.state;
 
@@ -37,10 +37,12 @@ export default class Todo extends Component {
         <input type="text" onInput={this.onInput} />
         <button onClick={this.addTodo}>Add ToDo</button>
         <ul>
-          { todos.map((todo, index) => <li key={index}>
-            {todo}
-            <button onClick={() => this.removeTodo(index)}>Remove</button>
-          </li>) }
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo}
+              <button onClick={() => this.removeTodo(index)}>Remove</button>
+            </li>
+          ))}
         </ul>
       </div>
     );
