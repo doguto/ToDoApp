c23eaa30cefa0b5f0183107bf0c4a0a4cd78f5f2
doguto <131944614+doguto@users.noreply.github.com>
2024-12-06 05:08:46 +0000

    [Update] Header's color

--- src/layout/pageHeader.jsx
+++ src/layout/pageHeader.jsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 export const PageHeader = () => {
   return (
-    <div className="w-full p-2 mb-4 bg-green-300">
+    <div className="w-full p-2 mb-4 bg-green-200">
       <h1 className="relative left-4 bottom-0.5 text-lg font-bold">TodoList</h1>
     </div>
   );
