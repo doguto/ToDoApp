7d1576a9e79cb67820caebcf055eebe247104b83
doguto <131944614+doguto@users.noreply.github.com>
2024-12-09 03:20:19 +0000

    [Feat] title base

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <main className="mx-auto mt-10 max-w-xl">
+      <h1 className="text-center text-4xl bg-lime-100">PTodo</h1>
+    </main>
+  );
+}
